b5ff4a59795d118154efbc5d1c573da77e669327
cameronrobertsdev <cameronroberts1998@gmail.com>
2018-11-02 17:25:50 -0700

    New Scripts added
    -Added zombie script
    -Added Player Vitals script

---
+++ GitHubGameOff_2018/Assets/Scripts/PlayerVitals.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVitals : MonoBehaviour {
+
+    public float health = 100;
+
+    public bool alive = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!alive)
+        {
+            Die();
+        }
+		
+	}
+
+    void Die()
+    {
+        //write stuff in here to activate things like death screens, stop player movement and shooting etc.
+
+    }
+}
---
+++ GitHubGameOff_2018/Assets/Scripts/Zombie.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour {
+
+    bool aware;
+
+    NavMeshAgent nav;
+
+    Transform playerTrans;
+
+    PlayerVitals playerVitals;
+
+	// Use this for initialization
+	void Start () {
+        nav = GetComponent<NavMeshAgent>();
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+        playerVitals = playerTrans.gameObject.GetComponent<PlayerVitals>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (aware)
+        {
+            nav.SetDestination(playerTrans.position);
+        }
+		
+	}
+
+    IEnumerator Damage()
+    {
+        while (true)
+        {
+
+            if (aware && Vector3.Distance(transform.position, playerTrans.position) < 1)
+            {
+                playerVitals.health -= 1;
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+}
