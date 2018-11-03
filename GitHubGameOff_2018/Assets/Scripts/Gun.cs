380eec7fcd1555bdbbdfbe1e28528abd4e838d4f
cameronrobertsdev <cameronroberts1998@gmail.com>
2018-11-02 17:01:27 -0700

    More Scripts(That I wrote)
    Don't worry I wrote these. With the exception of the "Gun.cs" script all scripts were written during the duration of the Game off.

---
+++ GitHubGameOff_2018/Assets/Scripts/Gun.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gun : MonoBehaviour {
+
+
+    float shoot;
+    bool triggerDown;
+    [SerializeField]
+    bool fullAuto;
+    [SerializeField]
+    int magSize;
+
+    [SerializeField]
+    float range;
+
+    [SerializeField]
+    float damage;
+
+    [SerializeField]
+    float fireRate;
+
+    [SerializeField]
+    float kick;
+
+    int currentMag;
+
+
+    public int totalAmmo;
+
+    [SerializeField]
+    int reloadTime;
+
+    int currentReloadTime;
+
+    bool reloading;
+
+    float reloadButton;
+
+    [SerializeField]
+    Text currentClipText;
+    [SerializeField]
+    Text totalAmmoText;
+    [SerializeField]
+    Text reloadText;
+
+    [SerializeField]
+    ParticleSystem hitParticles;
+
+    [SerializeField]
+    ParticleSystem muzzleFlash;
+
+    Transform cam;
+	// Use this for initialization
+	void Start () {
+        if (transform.parent.gameObject.tag != "Enemy")
+        {
+            cam = GetComponentInParent<Camera>().transform;
+        }
+        else
+        {
+            cam = transform.parent.transform;
+        }
+        currentMag = magSize;
+        if (fullAuto)
+        {
+            StartCoroutine(AutoFire());
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.tag == "Player1")
+        {
+            shoot = Input.GetAxisRaw("Fire1");
+            reloadButton = Input.GetAxisRaw("Reload");
+        }
+        if (gameObject.tag == "Player2")
+        {
+            shoot = Input.GetAxisRaw("Fire1P2");
+            reloadButton = Input.GetAxisRaw("ReloadP2");
+        }
+        totalAmmoText.text = totalAmmo.ToString();
+        currentClipText.text = currentMag.ToString();
+
+
+        if (!reloading && !triggerDown && currentMag < magSize && reloadButton == 1)
+        {
+            StartCoroutine( Reload());
+            reloading = true;
+        }
+
+        if (shoot == 1 && !triggerDown && currentMag > 0 && !reloading)
+        {
+            currentMag -= 1;
+            RaycastHit hit;
+            cam.Rotate(cam.InverseTransformDirection(cam.right) * kick);
+            if (Physics.Raycast(cam.position, cam.forward, out hit, range))
+            {
+                print("I hit: " + hit.transform.gameObject.name);
+                ParticleSystem hitInst;
+                hitInst = Instantiate(hitParticles,hit.point, new Quaternion(hit.normal.x, hit.normal.y, hit.normal.z, 0)) as ParticleSystem;
+                hitInst.Play();
+
+                if (hit.transform.gameObject.GetComponent<Rigidbody>() != null)
+                {
+                    Rigidbody tempRigid;
+                    tempRigid = hit.transform.gameObject.GetComponent<Rigidbody>();
+                    tempRigid.AddForceAtPosition(-hit.normal * 1000, hit.point);
+                }
+            }
+            triggerDown = true;
+        }
+        if (shoot == 0)
+        {
+            triggerDown = false;
+        }
+        if (reloading)
+        {
+            reloadText.enabled = true;
+        }
+        else
+        {
+            reloadText.enabled = false;
+        }
+
+        if (totalAmmo == 0)
+        {
+            reloadText.text = "OUT OF AMMO!";
+        }
+        else
+        {
+            reloadText.text = "RELOADING...";
+        }
+
+
+		
+	}
+
+    IEnumerator AutoFire()
+    {
+        while (true)
+        {
+            if (fullAuto)
+            {
+                if (currentMag > 0 && !reloading && triggerDown)
+                {
+                    currentMag -= 1;
+
+                    cam.Rotate(cam.InverseTransformDirection(cam.right) * kick);
+
+
+                    RaycastHit hit;
+                    if (Physics.Raycast(cam.position, cam.forward, out hit, range))
+                    {
+
+                        if (hit.transform.gameObject.GetComponent<Rigidbody>() != null)
+                        {
+                            Rigidbody tempRigid;
+                            tempRigid = hit.transform.gameObject.GetComponent<Rigidbody>();
+                            tempRigid.AddForceAtPosition(-hit.normal * 1000, hit.point);
+                        }
+
+                        print("I hit: " + hit.transform.gameObject.name);
+                        ParticleSystem hitInst;
+                        hitInst = Instantiate(hitParticles, hit.point, new Quaternion(hit.normal.x, hit.normal.y, hit.normal.z, 0)) as ParticleSystem;
+                        hitInst.Play();
+                    }
+
+                }
+            }
+            yield return new WaitForSeconds(fireRate);
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        print("Reload was called.");
+        yield return new WaitForSeconds(reloadTime);
+        totalAmmo -= magSize - currentMag;
+        currentMag = magSize;
+         if (totalAmmo < 0)
+         {
+            currentMag = currentMag + totalAmmo;
+            totalAmmo = 0;
+         }
+         reloading = false;
+    }
+
+}
--- GitHubGameOff_2018/Assets/Scripts/Move.cs
+++ GitHubGameOff_2018/Assets/Scripts/Move.cs
@@ -4,20 +4,122 @@ using UnityEngine;
 
 public class Move : MonoBehaviour {
 
+    Rigidbody rigid;
+    [SerializeField]
+    float speed;
 
-    float moveHori;
-    float moveVert;
+    [SerializeField]
+    float jumpStrength;
+
+    [SerializeField]
+    float maxVelocity;
+
+    float forward;
+    float right;
+    [SerializeField]
+    LayerMask groundLayerMask;
+
+    bool grounded;
+
+    float jump;
 
 	// Use this for initialization
 	void Start () {
-		
+        rigid = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        moveHori = Input.GetAxis("Horizontal");
-        moveVert = Input.GetAxis("Vertical");
-		
-	}
+
+        if (gameObject.tag == "Player1")
+        {
+            forward = Input.GetAxis("Vertical");
+            right = Input.GetAxis("Horizontal");
+            jump = Input.GetAxisRaw("Jump");
+        }
+        if (gameObject.tag == "Player2")
+        {
+            forward = Input.GetAxis("VerticalP2");
+            right = Input.GetAxis("HorizontalP2");
+            jump = Input.GetAxisRaw("JumpP2");
+        }
+
+        RaycastHit groundHit;
+        if (Physics.Raycast(transform.position, -transform.up, out groundHit, 1.1f, groundLayerMask))
+        {
+            grounded = true;
+            print("grounded");
+
+
+        }
+        else
+        {
+            grounded = false;
+            print("In Air");
+        }
+       
+    }
+
+    private void FixedUpdate()
+    {
+        if (grounded && jump == 1)
+        {
+            rigid.velocity = new Vector3(rigid.velocity.x, jumpStrength, rigid.velocity.z);
+        }
+
+        if (forward > 0.1f || forward < -0.1)
+        {
+            rigid.AddForce(transform.forward * forward * speed);
+            if (forward > 0.1 && transform.InverseTransformDirection(rigid.velocity).z < 0.1f)
+            {
+                rigid.AddForce(transform.forward * forward * speed * 2);
+            }
+            if (forward < -0.1 && transform.InverseTransformDirection(rigid.velocity).z > -0.1f)
+            {
+                rigid.AddForce(transform.forward * forward * speed * 2);
+            }
+        }
+        if (right > 0.1f || right < -0.1)
+        {
+            rigid.AddForce(transform.right * right * speed);
+            if (right > 0.1 && transform.InverseTransformDirection(rigid.velocity).x < 0.1f)
+            {
+                rigid.AddForce(transform.right * right * speed * 2);
+            }
+            if (right < -0.1 && transform.InverseTransformDirection(rigid.velocity).x > -0.1f)
+            {
+                rigid.AddForce(transform.right * right * speed * 2);
+            }
+        }
+
+        if (right == 0)
+        {
+            rigid.AddForce(transform.right * -transform.InverseTransformVector(rigid.velocity).x * 10);
+        }
+        if (forward == 0)
+        {
+            rigid.AddForce(transform.forward * -transform.InverseTransformVector(rigid.velocity).z * 10);
+        }
+
+        if (transform.InverseTransformVector(rigid.velocity).z > maxVelocity)
+        {
+            rigid.AddForce(transform.forward * -transform.InverseTransformVector(rigid.velocity).z * 10);
+        }
+        if (transform.InverseTransformVector(rigid.velocity).z < -maxVelocity)
+        {
+            rigid.AddForce(transform.forward * -transform.InverseTransformVector(rigid.velocity).z * 10);
+        }
+
+        if (transform.InverseTransformVector(rigid.velocity).x > maxVelocity)
+        {
+            rigid.AddForce(transform.right * -transform.InverseTransformVector(rigid.velocity).x * 10);
+        }
+        if (transform.InverseTransformVector(rigid.velocity).x < -maxVelocity)
+        {
+            rigid.AddForce(transform.right * -transform.InverseTransformVector(rigid.velocity).x * 10);
+        }
+
+    }
+
 }
